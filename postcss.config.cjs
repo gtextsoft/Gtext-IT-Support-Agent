02cf6440e78bb270b41f0105821f630b16aff48a
Olamilesi1 <olamilesiadeola111@gmail.com>
2025-08-08 22:32:36 +0100

    deleted node module

---
+++ postcss.config.cjs
@@ -0,0 +1,20 @@
+// // module.exports = {
+// //   plugins: {
+// //     tailwindcss: {},
+// //     autoprefixer: {},
+// //   },
+// // }
+
+// export default {
+//   plugins: {
+//     tailwindcss: {},
+//     autoprefixer: {},
+//   },
+// };
+
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+};
